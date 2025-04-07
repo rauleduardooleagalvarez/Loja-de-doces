d9c2e9416100739a599b5ce2350f923338535fc0
rauleduardooleagalvarez <hardcrack97@hotmail.com>
2025-04-07 14:54:32 -0400

    Update LojaDeDoces1.java

--- scr/LojaDeDoces1.java
+++ scr/LojaDeDoces1.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class LojaDeDoces {
+public class LojaDeDoces1 {
 
     public static void main(String[] args) {
         // Criando a janela
